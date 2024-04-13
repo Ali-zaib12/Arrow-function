ed13d2d71e9c305732c5b514477df26d5a39bbba
Ali-zaib12 <161868911+Ali-zaib12@users.noreply.github.com>
2024-04-13 14:24:25 +0500

    Add files via upload

---
+++ arrowfun.js
@@ -0,0 +1,10 @@
+// let arrowfunction = ()=>{
+//     console.log("Ali Zaib")
+// }
+// arrowfunction()
+var arrowfunction = function () {
+    var add = 4 + 5;
+    return add;
+};
+arrowfunction();
+console.log(arrowfunction());
---
+++ arrowfun.ts
@@ -0,0 +1,14 @@
+// let arrowfunction = ()=>{
+//     console.log("Ali Zaib")
+    
+// }
+// arrowfunction()
+
+let arrowfunction = ()=>{
+
+    let add = 4 + 5
+    return add
+}
+arrowfunction()
+
+console.log(arrowfunction())
